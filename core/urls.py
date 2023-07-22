d8ec31299549f3417fede1591183d932ca1b98af
MrMihen13 <kmerezhnikov2002@yandex.ru>
2023-07-22 18:28:39 +0500

    Added body filter

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from core import views
 
 
 urlpatterns = [
-    path('appeal', views.CreateAppealAPIView.as_view()),
+    path('appeal/', views.CreateAppealAPIView.as_view()),
     path('appeal/list/', views.ListAppealsAPIView.as_view()),
-    path('appeal/<int:pk>', views.RetrieveUpdateDeleteAppealAPIVew.as_view()),
+    path('appeal/<int:pk/>', views.RetrieveUpdateDeleteAppealAPIVew.as_view()),
 ]
